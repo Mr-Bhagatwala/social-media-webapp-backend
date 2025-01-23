089635dbd28179451c81caaa1856b0f22fdc6346
Vinit <vinits1014@gmail.com>
2025-01-23 17:18:21 +0530

    Added profile to message data fetched

--- Angular/src/app/components/chatApplication/chatApplication.component.js
+++ Angular/src/app/components/chatApplication/chatApplication.component.js
@@ -1,10 +1,38 @@
 "use strict"
 
-angular.module('myApp').controller("ParentController", function ($scope){
-    $scope.selectedChat = null; //initially no data
+angular.module('myApp').controller("ParentController", function ($scope, $http, $rootScope){
+    $scope.selectedChat = null; 
+    // let urlName = undefined;
+    let urlName = $rootScope.urlName;
+    const user_id = localStorage.getItem("user_id");
+
+    if (urlName != undefined) {
+        console.log("Called"); 
+        $scope.fetchChats = function () {
+            const url = `http://localhost/codeigniter/index.php/get-all-chats/${user_id}?searchQuery=${urlName}`;
+            $http
+                .get(url)
+                .then(function (response) {
+                    if (response.data.status === "success") {
+                        $scope.selectedChat = response.data.data[0];
+                        console.log(response.data.data); 
+                    } else {
+                        console.warn("No chats found");
+                    }
+                })
+                .catch(function (response) {
+                    console.error("Error in fetching chats:", response);
+                    alert("Error in fetching chats");
+                });
+        };
+        $scope.fetchChats();
+
+    }else{
+        console.log("Not called");
+    }
 
     $scope.setSelectedChat = function (chat) {
         $scope.selectedChat = chat;
     };
-
+    
 })
